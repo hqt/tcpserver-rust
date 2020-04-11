584db93ff0cae9411a9bdcec8dcc5061a4c3aa02
Huynh Quang Thao <thao.huynh@trustingsocial.com>
2020-04-12 05:29:00 +0700

    Using Arc for client_managers to share between threads

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,21 +1,32 @@
+use std::sync::atomic::AtomicUsize;
 use std::net::{TcpListener, TcpStream};
 use std::{io, thread};
 use std::io::{Read, Write};
 use crate::client_manager::ClientManager;
+use std::sync::Arc;
+use std::sync::atomic::Ordering::SeqCst;
+
+static test_port: AtomicUsize = AtomicUsize::new(1);
 
 // Server struct represents the server
 pub struct Server {
-    client_managers: ClientManager,
+    client_managers: Arc<ClientManager>,
     listener: Option<TcpListener>,
 }
 
 impl Server {
     pub fn new() -> Server {
         Server {
-            client_managers: ClientManager::new(),
+            client_managers: Arc::new(ClientManager::new()),
             listener: None,
         }
     }
+
+    // pub fn start_test_server() {
+    //     let addr = format!("127.0.0.1:{}", test_port.fetch_add(1, SeqCst));
+    //     thread::spawn(move || {
+    //     });
+    // }
 
     pub fn start_server(&mut self, addr: &str) -> Result<(), io::Error> {
         let result = TcpListener::bind(addr);
@@ -33,8 +44,9 @@ impl Server {
         for stream in self.listener.as_ref().unwrap().incoming() {
             match stream {
                 Ok(stream) => {
+                    let client_manager = self.client_managers.clone();
                     thread::spawn(move || {
-                        handle_client(stream);
+                        handle_client(client_manager, stream)
                     });
                 }
                 Err(e) => {
@@ -45,7 +57,7 @@ impl Server {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(client_manager: Arc<ClientManager>, mut stream: TcpStream) {
     println!("client");
     // read 20 bytes at a time from stream echoing back to stream
     loop {
